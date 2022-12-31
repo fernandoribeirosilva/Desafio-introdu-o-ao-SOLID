4e07fcbfba825b0896fa14c2955678ef5d1c6988
fernandoribeirosilva <fernando1996ribeirosilva@gmail.com>
2022-12-31 15:18:41 -0300

    feito a documentação a api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 export { app };
